7417ac1ce9dee5195386f6c236cb8276856f48cd
scandale <scandale89@gmail.com>
2019-08-01 00:55:25 +0300

    Revert "Done"
    This reverts commit b1b479762907b16c9f323bd715a77a66e3e15515.

--- src/fruits/Apple.java
+++
@@ -1,7 +0,0 @@
-package fruits;
-
-public class Apple extends Fruit {
-    float weight = 1.0f;
-
-
-}
--- src/fruits/Orange.java
+++
@@ -1,6 +0,0 @@
-package fruits;
-
-public class Orange extends Fruit   {
-    float weight = 1.5f;
-
-}
